40e9d1102933c04e0faa1574857cc917eb614a6d
felipeghidini <lipeghidini18@gmail.com>
2024-05-30 22:53:02 -0300

    feat: incluido a configuracao com o servico de envio de email

--- src/main/java/com/microservice/email/consumers/EmailConsumer.java
+++ src/main/java/com/microservice/email/consumers/EmailConsumer.java
@@ -1,16 +1,26 @@
 package com.microservice.email.consumers;
 
 import com.microservice.email.dtos.EmailRecordDto;
-import org.hibernate.boot.model.internal.QuerySecondPass;
+import com.microservice.email.models.EmailModel;
+import com.microservice.email.services.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
 
+    @Autowired
+    EmailService emailService;
+
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto) {
-        System.out.println(emailRecordDto.emailTo());
+        var emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailRecordDto, emailModel);
+
+        // send email
+        emailService.sendEmail(emailModel);
     }
 }
